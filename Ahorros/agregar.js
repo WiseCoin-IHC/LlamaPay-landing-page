979fbdb62bb6372f0b1e3b42a2da51fd5a2a7885
Jenn305 <jenniespinoza2002@hotmail.com>
2023-06-07 19:32:12 -0500

    feat:ahorros-page-added

---
+++ Ahorros/agregar.js
@@ -0,0 +1,54 @@
+import Formulario from "./formulario.js";
+
+const btnAbrirPopUP = document.querySelector("#btn-new");
+const btnCerrarPopUP = document.querySelector("#btn-close");
+
+const modal =  document.querySelector("#modal");
+
+const elem = document.querySelector("#Objetivos")
+
+btnAbrirPopUP.addEventListener("click", ()=>{
+    modal.showModal();
+});
+
+btnCerrarPopUP.addEventListener("click", ()=> {
+    modal.close();
+});
+
+const formulario = document.querySelector("#formulario");
+
+formulario.addEventListener("submit", event=>{
+    event.preventDefault();
+    const target = event.target;
+
+    const agregar = new Formulario(target.nombreFormulario.value, target.montoFormulario.value, target.fechaFormulario.value, target.descripcionFormulario.value);
+    mostrar(agregar);
+    formulario.reset(); // resetea el formulario
+});
+
+let IdContador = 0;
+
+function mostrar(obj) {
+    IdContador++;
+
+    Objetivos.innerHTML += `
+    <div class="card-Objetivos" id="${IdContador}">
+        <img src="../Img/x-lg.svg" alt="Eliminar">
+        <h3>${obj.getNombre()}</h3>
+        <p>El Monto a Ahorrar es: S/.${obj.getMonto()}</p>
+        <p>La Fecha Límite es: ${obj.getFecha()}</p>
+        <p>${obj.getDescripcion()}</p>
+    </div>
+    `
+};
+
+Objetivos.addEventListener("click", (event)=>{
+    if(event.srcElement.nodeName == "IMG"){
+        eliminar(event.target.parentNode.id);
+    };
+});
+
+let eliminar = (id) => {
+    let ObjEliminar = document.getElementById(id);
+    Objetivos.removeChild(ObjEliminar);
+}
