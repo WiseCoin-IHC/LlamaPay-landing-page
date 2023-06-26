a6cdb16627c333fa697b22c738c86a6131f0a586
DiegoDefSan <defsan35@gmail.com>
2023-06-26 11:07:44 -0500

    feat:verification_reminders_added

--- public/assets/scripts/reminders/reminder.form.js
+++ public/assets/scripts/reminders/reminder.form.js
@@ -4,6 +4,8 @@ const inputAmount = document.getElementById('amount-input');
 const inputDate = document.getElementById('date-input');
 const inputPlace = document.getElementById('place-input');
 const inputDetails = document.getElementById('details-input');
+const invalidData = document.getElementById('invalid-data');
+const invalidDate = document.getElementById('invalid-date');
 
 function displayForm() {
     formContainer.style.left = '0'
@@ -15,6 +17,8 @@ function emptyForm() {
     inputDate.value='';
     inputPlace.value='';
     inputDetails.value='';
+
+    
 }
 
 function contractFrom() {
@@ -23,17 +27,52 @@ function contractFrom() {
     formContainer.style.left = '-100%'
 }
 
+function verifyDate() {
+    let dateReminder = inputDate.value;
+
+    let fechaSeleccionada = new Date(dateReminder);
+    let fechaActual=new Date();
+
+    if (fechaSeleccionada>fechaActual) return true;
+    return false;
+}
+
+function verifyData() {
+    let nombreReminder = inputNombre.value;
+    let amountReminder = inputAmount.value;
+    let dateReminder = inputDate.value;
+
+    if (nombreReminder==='') return false;
+    if (amountReminder==='') return false;
+    if (dateReminder==='') return false;
+
+    return true;
+}
 
 
 function acceptButton() {
+    invalidData.style.display='none';
+    invalidDate.style.display='none';
+
     let nombreReminder = inputNombre.value;
     let amountReminder = inputAmount.value;
     let dateReminder = inputDate.value;
     let placeReminder = inputPlace.value;
     let detailsReminder = inputDetails.value;
 
-    contractFrom();
+    if (verifyData() && verifyDate()) {
+        
+        contractFrom();
+    
+        addData(nombreReminder, amountReminder, dateReminder, placeReminder, detailsReminder);
+        addReminder();
+        return;
+    }
+    if (!verifyData()) {
+        invalidData.style.display='block';
+    }
+    if (!verifyDate()) {
+        invalidDate.style.display='block';
+    }
 
-    addData(nombreReminder, amountReminder, dateReminder, placeReminder, detailsReminder);
-    addReminder();
 }
